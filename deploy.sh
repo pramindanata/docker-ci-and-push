24f0b3ec3bb727719eb2ef61b00c1e426940a2b0
pramindanata <pramindanata.eksa@gmail.com>
2020-07-03 17:27:02 +0800

    bjir

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,11 @@ pat="^([0-9]+)\.([0-9]+)\.([0-9]+)(-([0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*))?(\+[0-9A-
 
 if [[ $version =~ $pat ]]
 then
-  echo "No deployment. Commit message doesn't contain certain value."
-else
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   docker build -t "$DOCKER_IMAGE_NAME":latest .
   docker tag "$DOCKER_IMAGE_NAME":latest "$DOCKER_IMAGE_NAME":"$version"
   docker push "$DOCKER_IMAGE_NAME":"$version"
   docker push "$DOCKER_IMAGE_NAME":latest
+else
+  echo "No deployment. Commit message doesn't contain certain value."
 fi
